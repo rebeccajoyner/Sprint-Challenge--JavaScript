c837baf5c55cf32765dd5dccca940cab77a6f344
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-17 15:35:46 -0400

    classes1

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,41 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+
+class CuboidMaker {
+    constructor(length, width, height){   ////constructor() is foundation of class; LWH attributes bound to obj as properties which are passed in thru constructor; classes will return objects
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    
+    }
+}
+
+
+
+  CuboidMaker.prototype.volume = function () {
+    return `${this.length} * ${this.width} * ${this.height}`
+  }
+
+  CuboidMaker.prototype.surfaceArea = function () {
+    return (`2 * (${this.length} * ${this.width} + ${this.length} * ${this.height} + ${this.width} * ${this.height}`)
+  }
+
+  const cuboid = new CuboidMaker({
+    length: 4,
+    width: 5,
+    height: 5
+  });
+
+
+
+
+
+
+
+
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
